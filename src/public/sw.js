c6a5745915e88d6b1ee971c54bd727bf58469b1a
abdul-razak alhassan <aabdulrazak22@gmail.com>
2026-02-28 04:48:23 +0000

    public folder add

---
+++ src/public/sw.js
@@ -0,0 +1,33 @@
+// Basic Service Worker for App Shell Caching
+const CACHE_NAME = 'farmops-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  // Stale-while-revalidate strategy for UI assets
+  if (event.request.mode === 'navigate' || event.request.destination === 'script' || event.request.destination === 'style') {
+    event.respondWith(
+      caches.match(event.request).then((cachedResponse) => {
+        const fetchPromise = fetch(event.request).then((networkResponse) => {
+          caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request, networkResponse.clone());
+          });
+          return networkResponse;
+        });
+        return cachedResponse || fetchPromise;
+      })
+    );
+  }
+});
